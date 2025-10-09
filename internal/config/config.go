bf75abd45ef935e14f7283a66a7ce8dfb27a8956
shavits <s.shavit4@gmail.com>
2025-10-09 08:29:39 +0300

    added read json to config package

---
+++ internal/config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+
+type Config struct{
+    DbURL           string `json:"db_url"`
+    CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error){
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer configFile.Close()
+
+	dec := json.NewDecoder(configFile)
+	var config Config
+	if err := dec.Decode(&config); err != nil{
+		return Config{}, err
+	}
+	return config, nil
+
+}
+
+func SetUser(userName string) error{
+	
+}
+
+
+
+
+func getConfigFilePath() (string, error){
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("Failed to get config file path: %s", err)
+	}
+
+	return fmt.Sprintf("%s/%s", homeDir, configFileName), nil
+
+}
+
+func write(cfg Config) error
+
